e08a2c378c65e83ca8aaffb5be1df10fe0cf22c9
lai-chun-lin <axd5201211@gmail.com>
2019-10-18 22:05:12 +0800

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,22 @@ use Illuminate\Http\Request;
 //    return view('welcome');
 //});
 //顯示所有任務的清單
-Route::get('/', function () {
-    return view('tasks');
-});
+//Route::get('/', function () {
+//    return view('tasks');
+//});
 // 增加新的任務
 //Route::post('/task', function (Request $request) {
 ////
 //});
+
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+//利用model Task由DB的tasks資料表取出資料並排序
+//暫存 $tasks
+//可簡化為 $tasks= Task->get();
+    return view('tasks', [ 'tasks' => $tasks ]);
+//將取出的資料$tasks傳遞給tasks視圖
+});
 
 Route::post('/task', function (Request $request) {
 // 驗證輸入
@@ -44,6 +53,12 @@ Route::post('/task', function (Request $request) {
 
 
 // 刪除任務
+//Route::delete('/task/{task}', function (Task $task) {
+////
+//});
+
+
 Route::delete('/task/{task}', function (Task $task) {
-//
+    $task->delete();
+    return redirect('/');
 });
