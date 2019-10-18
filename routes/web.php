e54ec435fea7fcb80fba8eba29132f5267e3ae4c
lai-chun-lin <axd5201211@gmail.com>
2019-10-18 21:41:54 +0800

    create three routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+// 增加新的任務
+Route::post('/task', function (Request $request) {
+//
+});
+// 刪除任務
+Route::delete('/task/{task}', function (Task $task) {
+//
 });
