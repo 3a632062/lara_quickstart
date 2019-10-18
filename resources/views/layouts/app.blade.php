b2b764bc738e16bf4e884f3b1243be9721f97d95
lai-chun-lin <axd5201211@gmail.com>
2019-10-18 21:50:41 +0800

    create layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+// resources/views/layouts/app.blade.php
+
+<html lang="en">
+<head>
+    <title>Laravel 快速入門 - 基本</title>
+
+    <!-- CSS 及 JavaScript -->
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar 內容 -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
