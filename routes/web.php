71f7de71027c88b4c1bae65d67de3e369bd90326
lai-chun-lin <axd5201211@gmail.com>
2019-10-18 21:58:19 +0800

    insert task and data validator

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,27 @@ Route::get('/', function () {
     return view('tasks');
 });
 // 增加新的任務
+//Route::post('/task', function (Request $request) {
+////
+//});
+
 Route::post('/task', function (Request $request) {
-//
+// 驗證輸入
+    $validator = Validator::make($request->all() , [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
 });
+
+
 // 刪除任務
 Route::delete('/task/{task}', function (Task $task) {
 //
